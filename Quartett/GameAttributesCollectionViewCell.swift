7c4df87d5ac0d37f5f18d3d6f497fd338a1a2896
Moritz Martin <moritz.martin@uni-ulm.de>
2016-01-05 15:52:58 +0100

    CollectionView

---
+++ Quartett/GameAttributesCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  GameAttributesCollectionViewCell.swift
+//  Quartett
+//
+//  Created by Moritz Martin on 05.01.16.
+//  Copyright © 2016 Moritz Martin. All rights reserved.
+//
+
+import UIKit
+
+class GameAttributesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var attributeIcon: UIImageView!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+}
