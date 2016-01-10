9cceccba0240724992e593f813e7bcac2cac79b4
Baschdi <baschdi@online.de>
2016-01-10 20:41:43 +0100

    Constraints vom End Screen View

--- Quartett/EndScreenViewController.swift
+++ Quartett/EndScreenViewController.swift
@@ -13,17 +13,20 @@ import CoreData
 class EndScreenViewController: UIViewController {
 
     
+    @IBOutlet weak var mainMenuButton: UIButton!
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var rankingButton: UIButton!
+    
+    
     @IBOutlet weak var gameOverImage: UIImageView!
-    @IBOutlet weak var gameOverText: UITextView!
+    @IBOutlet weak var gameOverText: UILabel!
     
     var labelTxt: String = "Game Over Text"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         gameOverText.text = labelTxt
-//        gameOverText.selectable = false
         if(gameOverText.text == "Du hast gewonnen!"){
             gameOverImage.image = UIImage(named: "winLogo")
         }else if(gameOverText.text == "Unentschieden!"){
@@ -32,6 +35,14 @@ class EndScreenViewController: UIViewController {
         else{
             gameOverImage.image = UIImage(named: "looseLogo")
         }
+        
+        rankingButton.layer.borderWidth = 2
+        mainMenuButton.layer.borderWidth = 2
+        newGameButton.layer.borderWidth = 2
+        
+        rankingButton.layer.borderColor = UIColorFromRGB(0x209624).CGColor
+        mainMenuButton.layer.borderColor = UIColorFromRGB(0x209624).CGColor
+        newGameButton.layer.borderColor = UIColorFromRGB(0x209624).CGColor
 
         // Do any additional setup after loading the view.
     }
@@ -41,6 +52,15 @@ class EndScreenViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
   
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 
     /*
     // MARK: - Navigation
